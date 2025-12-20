cb795800ba90877aabb74588c8988cbcc07ee51b
Juan Royo de Barrio <juanroyofullstack@gmail.com>
2025-12-20 23:29:48 +0100

    add: local image and render in component

--- app/src/main/java/com/example/myapplication/ui/components/WelcomeImage.kt
+++ app/src/main/java/com/example/myapplication/ui/components/WelcomeImage.kt
@@ -1,13 +1,34 @@
 package com.example.myapplication.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.R
 
 @Composable
 fun ImageWelcome() {
-        Text(
-            text = "Contenido de imagen",
-            color = MaterialTheme.colorScheme.onBackground
+    Text(
+        text = "Contenido de imagen",
+        color = MaterialTheme.colorScheme.onBackground
+    )
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.welcome_image),
+            contentDescription = "Imagen de bienvenida",
+            modifier = Modifier.size(200.dp),
+            contentScale = ContentScale.Crop
         )
+    }
 }
