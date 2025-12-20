600e904ad00f751c872774b25b4d3589a1d2bb38
Juan Royo de Barrio <juanroyofullstack@gmail.com>
2025-12-20 23:51:35 +0100

    remove: unnecesary imports

--- app/src/main/java/com/example/myapplication/ui/components/MyAppBar.kt
+++ app/src/main/java/com/example/myapplication/ui/components/MyAppBar.kt
@@ -1,19 +1,11 @@
 package com.example.myapplication.ui.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MyAppBar() {
-    var menuExpanded by remember { mutableStateOf(false) }
 
     TopAppBar(
         title = { Text("Mi Header") },
