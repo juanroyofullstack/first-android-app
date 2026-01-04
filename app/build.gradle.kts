3f1a6e4f8d522c73f6c59aa12ca1fd830eca4fbb
Juan Royo de Barrio <juanroyofullstack@gmail.com>
2026-01-04 16:06:25 +0100

    add: refacto and identation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,10 +51,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity)
     implementation(libs.material)
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.navigation.compose)
 
     // Compose dependencies
     implementation(libs.androidx.activity.compose)
@@ -63,15 +64,15 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation("androidx.navigation:navigation-compose:2.8.0")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    
+
     // Compose testing
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
 }
--- app/src/main/java/com/example/myapplication/ui/components/AppContent.kt
+++ app/src/main/java/com/example/myapplication/ui/components/AppContent.kt
@@ -2,36 +2,36 @@ package com.example.myapplication.ui.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.myapplication.model.NewsItem
 import com.example.myapplication.model.NewsUiState
-import com.example.myapplication.viewmodel.NewsViewModel
-import com.example.myapplication.ui.screens.NewsDetail
 
 @Composable
 fun AppContent(
@@ -39,7 +39,6 @@ fun AppContent(
     newsState: NewsUiState,
     onSearch: (String) -> Unit,
     onNewsClick: (NewsItem) -> Unit,
-    viewModel: NewsViewModel = viewModel(),
 ) {
     var searchText by remember { mutableStateOf("") }
 
@@ -47,16 +46,24 @@ fun AppContent(
         modifier = Modifier
             .padding(innerPadding)
             .fillMaxSize()
-            .padding(16.dp, 36.dp, 16.dp, 16.dp)
+            .padding(16.dp, 46.dp, 16.dp, 16.dp)
     ) {
         Spacer(modifier = Modifier.height(64.dp))
 
-        OutlinedTextField(
+        TextField(
             value = searchText,
             onValueChange = { searchText = it },
             modifier = Modifier.fillMaxWidth(),
             placeholder = { Text("Buscar...") },
             singleLine = true,
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onSearch(searchText)
+                }
+            ),
             trailingIcon = {
                 IconButton(onClick = { onSearch(searchText) }) {
                     Icon(
@@ -67,17 +74,17 @@ fun AppContent(
             }
         )
         if (newsState.isLoading) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            CircularProgressIndicator()
-        }
-    } else if (newsState.error != null) {
-        Text(text = newsState.error, color = Color.Red)
-    } else {
-            if(newsState.articles.isEmpty()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        } else if (newsState.error != null) {
+            Text(text = newsState.error, color = Color.Red)
+        } else {
+            if (newsState.articles.isEmpty()) {
                 Text(
                     text = "Mi primera aplicación android",
                     style = MaterialTheme.typography.headlineLarge,
@@ -90,14 +97,16 @@ fun AppContent(
                 )
                 ImageWelcome()
             }
-            if(newsState.articles.isNotEmpty()) {
+            if (newsState.articles.isNotEmpty()) {
                 newsState.articles.forEach { article ->
                     Text(
                         text = article.title,
                         style = MaterialTheme.typography.bodyLarge,
-                        modifier = Modifier.padding(vertical = 8.dp).clickable {
-                            onNewsClick(article)
-                        },
+                        modifier = Modifier
+                            .padding(vertical = 8.dp)
+                            .clickable {
+                                onNewsClick(article)
+                            },
                     )
                 }
             }
